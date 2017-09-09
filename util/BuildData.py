0600ad876c8d4c21ce5496a4c8a554103db61c09
iosandroid <vadzim.ziankovich@gmail.com>
2017-09-09 05:57:52 -0700

    [notebook] updated

--- util/BuildData.py
+++ util/BuildData.py
@@ -193,71 +193,71 @@ def BuildData2(zigzag, returns, lag):
 
     return S
 
-#def BuildData3(zigzag, returns, lag):
-#    
-#    D = []
-#    L = []
-#    
-#    D0 = []
-#    D1 = []
+def BuildData3(zigzag, returns, lag):
     
-#    L0 = []
-#    L1 = []
+    D = []
+    L = []
     
-#    N = len(returns) + 1
-#    Count = len(zigzag['time'])
+    D0 = []
+    D1 = []
+    
+    L0 = []
+    L1 = []
+    
+    N = len(returns) + 1
+    Count = len(zigzag['time'])
 
-#    for i in range(0,N-lag):        
-#        try:
-#            index = zigzag["time"].index(i+lag-1)
-#            
-#            D0.append(returns[i:i+lag-1])
-#            
-#            if zigzag["label"][index] == 1:                
-#                L0.append([0, 0, 1])
-#            else:
-#                L0.append([1, 0, 0])
+    for i in range(0,N-lag):        
+        try:
+            index = zigzag["time"].index(i+lag-1)
             
-#        except:
-#            
-#            D1.append(returns[i:i+lag-1])
-#            L1.append([0, 1, 0])
+            D0.append(returns[i:i+lag-1])
+            
+            if zigzag["label"][index] == 1:                
+                L0.append([0, 0, 1])
+            else:
+                L0.append([1, 0, 0])
+            
+        except:
+            
+            D1.append(returns[i:i+lag-1])
+            L1.append([0, 1, 0])
 
-#    Count0 = len(D0)
-#    Count1 = len(D1)
+    Count0 = len(D0)
+    Count1 = len(D1)
     
-#    #print(Count0, Count1)
+    #print(Count0, Count1)
     
-#    N = Count0 / 2
-#    if Count1 > N:        
-#        D1 = np.random.permutation(D1)
+    N = Count0 / 2
+    if Count1 > N:        
+        D1 = np.random.permutation(D1)
         
-#        D = D1[:N]
-#        L = L1[:N]
+        D = D1[:N]
+        L = L1[:N]
         
-#    else:        
-#        D = D1
-#        L = L1
+    else:        
+        D = D1
+        L = L1
     
-#    D_temp = np.concatenate((D, D0), axis=0)
-#    L_temp = np.concatenate((L, L0), axis=0)
+    D_temp = np.concatenate((D, D0), axis=0)
+    L_temp = np.concatenate((L, L0), axis=0)
     
-#    I = range(len(D_temp))
-#    I = np.random.permutation(I)
+    I = range(len(D_temp))
+    I = np.random.permutation(I)
+   
+    D = []
+    L = []
     
-#    D = []
-#    L = []
-    
-#    for i in I:
-#        D.append(D_temp[i])
-#        L.append(L_temp[i])
+    for i in I:
+        D.append(D_temp[i])
+        L.append(L_temp[i])
      
-#    D = np.array(D)
-#    L = np.array(L)   
+    D = np.array(D)
+    L = np.array(L)   
     
-#    D = preprocessing.scale(D)
+    D = preprocessing.scale(D)
     
-#    return D, L
+    return D, L
 
 def BuildData4(zigzag, returns, lag):
     
