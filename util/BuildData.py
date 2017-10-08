73d88cb6df4f6a2a600ce18126443c8224d1a527
iosandroid <vadzim.ziankovich@gmail.com>
2017-10-07 17:34:16 -0700

    [notebook] refactored generating training set

--- util/BuildData.py
+++ util/BuildData.py
@@ -1,290 +1,258 @@
 import numpy as np
-from util import ZigZag
-from sklearn import preprocessing
 
-#def BuildData0(zigzag, data, lag):
+import matplotlib.pyplot as plt
+
+from scipy.linalg import hankel
+
+from util.ZigZag import ZigZag
+from util.CalcReturns import CalcReturns
+
+
+#from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+
+def BuildTrainSetForMLUsingPrices(prices, minsize, lag):   
+
+    S = MinMaxScaler()
+    P = prices
     
+    #P = S.fit_transform(P)
+    #M = S.transform([minsize])
+    M = minsize
+
+    Z = ZigZag(P, M)
+
+    N = len(P)
+    T = { 'input' : hankel(P[0 : lag], P[lag-1 :]).T, 'label' : np.full(N-lag, 0) }
+
+    for i in range(len(Z)):
+        tmin = Z[i]['tmin']
+        tmax = Z[i]['tmax']
+
+        T['label'][tmin:tmax] = np.full(tmax - tmin, Z[i]['label'])
+
+    return T, S
+
+def BuildTrainSetForMLUsingReturns(prices, minsize, lag):   
+
+    P = prices
+    N = len(P)
+
+    T0, S = BuildTrainSetForMLUsingPrices(prices, minsize, lag + 1)
+    T = { 'input' : np.full((N-lag, lag), 0), 'label' : T0['label'] }
+
+    for i in range(len(T0['input'])):
+        T['input'][i] = CalcReturns(T0['input'][i])
+
+    return T, S
+
+
+# Build data without trend removing
+#def BuildData0(zigzag, prices):
+#    
+#    scaler = MinMaxScaler()
+
 #    D = []
 #    L = []
-    
-#    D0 = []
-#    D1 = []
-    
-#    L0 = []
-#    L1 = []
-    
-#    N = len(data)
-#    Count = len(Z['time'])
 
-#    for i in range(0,N-lag):        
-#        try:
-#            index = zigzag["time"].index(i+lag)
-            
-#            D0.append(data[i:i+lag])
-#            L0.append(zigzag["label"][index])
-            
-#        except:
-            
-#            D1.append(data[i:i+lag])
-#            L1.append(0)            
+#    for i in range(len(zigzag["time"])-1):
+#        index0 = zigzag["time"][i + 0]
+#        index1 = zigzag["time"][i + 1]
 
-#    Count0 = len(D0)
-#    Count1 = len(D1)
-    
-#    #print(Count0, Count1)
-    
-#    N = Count0 / 2
-#    if Count1 > N:        
-#        D1 = np.random.permutation(D1)
+#        label  = zigzag["label"][i + 0]
         
-#        D = D1[:N]
-#        L = L1[:N]
-        
-#    else:        
-#        D = D1
-#        L = L1
-    
-#    D_temp = np.concatenate((D, D0), axis=0)
-#    L_temp = np.concatenate((L, L0), axis=0)    
-    
-#    I = range(len(D_temp))
-#    I = np.random.permutation(I)
-    
-#    D = []
-#    L = []
-    
-#    for i in I:
-#        D.append(D_temp[i])
-#        L.append(L_temp[i])
-    
+#        for j in range(index0, index1):
+#            D.append(prices[j])
+#            L.append([0, 1] if label == 1 else [1, 0])
+
+#    N = len(D)
+
 #    D = np.array(D)
-#    L = np.array(L)   
+#    L = np.array(L)
+
+#    D = scaler.fit_transform(D)
+
+#    #### plot #############################################
+#    fig, ax = plt.subplots()
+#    for i in range(len(zigzag["time"])-1):
+#        i0 = zigzag["time"][i + 0]
+#        i1 = zigzag["time"][i + 1]
+
+#        lb = zigzag["label"][i + 0]
+
+#        ax.plot(range(i0,i1), D[i0:i1], color = 'green' if lb == 1 else 'red')
+
+#    plt.show()
+#    ######################################################
+
+
+#    #D = D.reshape(N,1,1)
+
+#    T = {'input' : [], 'label' : []}
     
-#    D = preprocessing.scale(D)
-    
-#    return D, L
-    
-#def BuildData1(zigzag, returns, lag):
-    
+#    T['input'] = D
+#    T['label'] = L    
+
+#    return T, scaler
+
+#def BuildData2(zigzag, returns, lag):
+#    
 #    D = []
 #    L = []
-    
+#    
 #    D0 = []
 #    D1 = []
-    
+#    
 #    L0 = []
 #    L1 = []
     
 #    N = len(returns) + 1
-#    Count = len(Z['time'])
+#    Count = len(zigzag['time'])
 
 #    for i in range(0,N-lag):        
 #        try:
 #            index = zigzag["time"].index(i+lag-1)
-            
+#            
 #            D0.append(returns[i:i+lag-1])
 #            L0.append(zigzag["label"][index])
-            
+#            
 #        except:
-            
+#            
 #            D1.append(returns[i:i+lag-1])
 #            L1.append(0)            
-
+#
 #    Count0 = len(D0)
 #    Count1 = len(D1)
-    
+#    
 #    #print(Count0, Count1)
-    
-#    N = Count0 / 2
-#    #N = Count0
+#    
+#    N = Count0
 #    if Count1 > N:        
 #        D1 = np.random.permutation(D1)
-        
+#        
 #        D = D1[:N]
 #        L = L1[:N]
-        
+#        
 #    else:        
 #        D = D1
 #        L = L1
-    
+#    
 #    D_temp = np.concatenate((D, D0), axis=0)
 #    L_temp = np.concatenate((L, L0), axis=0)
-    
+#    
 #    I = range(len(D_temp))
 #    I = np.random.permutation(I)
-    
+#    
+#    #D = []
+#    #L = []
+#
+#    S = {'data' : [], 'label' : [], 'index' : []}
+#    
+#    for i in I:
+#        S['index'].append(i)
+#        S['data'].append(D_temp[i])
+#        S['label'].append(L_temp[i])
+#    
+#    S['data']  = np.array(S['data'])
+#    S['label'] = np.array(S['label'])
+#    S['index'] = np.array(S['index'])
+#    
+#    S['data'] = preprocessing.scale(S['data'])
+#
+#    return S
+
+#def BuildData3(zigzag, returns, lag):
+#    
 #    D = []
 #    L = []
-    
+#    
+#    D0 = []
+#    D1 = []
+#    
+#    L0 = []
+#    L1 = []
+#    
+#    N = len(returns) + 1
+#    Count = len(zigzag['time'])
+#
+#    for i in range(0,N-lag):        
+#        try:
+#            index = zigzag["time"].index(i+lag-1)
+#            
+#            D0.append(returns[i:i+lag-1])
+#            
+#            if zigzag["label"][index] == 1:                
+#                L0.append([0, 0, 1])
+#            else:
+#                L0.append([1, 0, 0])
+#            
+#        except:
+#            
+#            D1.append(returns[i:i+lag-1])
+#            L1.append([0, 1, 0])
+#
+#    Count0 = len(D0)
+#    Count1 = len(D1)
+#    
+#    #print(Count0, Count1)
+#    
+#    N = Count0 / 2
+#    if Count1 > N:        
+#        D1 = np.random.permutation(D1)
+#        
+#        D = D1[:N]
+#        L = L1[:N]
+#        
+#    else:        
+#        D = D1
+#        L = L1
+#    
+#    D_temp = np.concatenate((D, D0), axis=0)
+#    L_temp = np.concatenate((L, L0), axis=0)
+#    
+#    I = range(len(D_temp))
+#    I = np.random.permutation(I)
+#   
+#    D = []
+#    L = []
+#    
 #    for i in I:
 #        D.append(D_temp[i])
 #        L.append(L_temp[i])
-    
+#     
 #    D = np.array(D)
 #    L = np.array(L)   
-    
+#    
 #    D = preprocessing.scale(D)
-    
+#    
 #    return D, L
 
-def BuildData2(zigzag, returns, lag):
-    
-    D = []
-    L = []
-    
-    D0 = []
-    D1 = []
-    
-    L0 = []
-    L1 = []
-    
-    N = len(returns) + 1
-    Count = len(zigzag['time'])
+#def BuildData4(zigzag, returns, lag):
+#    
+#    D = []
+#    L = []
+#  
+#    N = len(returns) + 1
+#    Count = len(zigzag['time'])
 
-    for i in range(0,N-lag):        
-        try:
-            index = zigzag["time"].index(i+lag-1)
-            
-            D0.append(returns[i:i+lag-1])
-            L0.append(zigzag["label"][index])
-            
-        except:
-            
-            D1.append(returns[i:i+lag-1])
-            L1.append(0)            
-
-    Count0 = len(D0)
-    Count1 = len(D1)
-    
-    #print(Count0, Count1)
-    
-    N = Count0
-    if Count1 > N:        
-        D1 = np.random.permutation(D1)
+#    #returns = preprocessing.scale(returns)
+#
+#    for i in range(len(zigzag["time"])-1):
+#        index0 = zigzag["time"][i + 0]
+#        index1 = zigzag["time"][i + 1]
+#
+#        l = zigzag["label"][i + 0]
         
-        D = D1[:N]
-        L = L1[:N]
-        
-    else:        
-        D = D1
-        L = L1
-    
-    D_temp = np.concatenate((D, D0), axis=0)
-    L_temp = np.concatenate((L, L0), axis=0)
-    
-    I = range(len(D_temp))
-    I = np.random.permutation(I)
-    
-    #D = []
-    #L = []
-
-    S = {'data' : [], 'label' : [], 'index' : []}
-    
-    for i in I:
-        S['index'].append(i)
-        S['data'].append(D_temp[i])
-        S['label'].append(L_temp[i])
-    
-    S['data']  = np.array(S['data'])
-    S['label'] = np.array(S['label'])
-    S['index'] = np.array(S['index'])
-    
-    S['data'] = preprocessing.scale(S['data'])
-
-    return S
-
-def BuildData3(zigzag, returns, lag):
-    
-    D = []
-    L = []
-    
-    D0 = []
-    D1 = []
-    
-    L0 = []
-    L1 = []
-    
-    N = len(returns) + 1
-    Count = len(zigzag['time'])
-
-    for i in range(0,N-lag):        
-        try:
-            index = zigzag["time"].index(i+lag-1)
-            
-            D0.append(returns[i:i+lag-1])
-            
-            if zigzag["label"][index] == 1:                
-                L0.append([0, 0, 1])
-            else:
-                L0.append([1, 0, 0])
-            
-        except:
-            
-            D1.append(returns[i:i+lag-1])
-            L1.append([0, 1, 0])
-
-    Count0 = len(D0)
-    Count1 = len(D1)
-    
-    #print(Count0, Count1)
-    
-    N = Count0 / 2
-    if Count1 > N:        
-        D1 = np.random.permutation(D1)
-        
-        D = D1[:N]
-        L = L1[:N]
-        
-    else:        
-        D = D1
-        L = L1
-    
-    D_temp = np.concatenate((D, D0), axis=0)
-    L_temp = np.concatenate((L, L0), axis=0)
-    
-    I = range(len(D_temp))
-    I = np.random.permutation(I)
-   
-    D = []
-    L = []
-    
-    for i in I:
-        D.append(D_temp[i])
-        L.append(L_temp[i])
-     
-    D = np.array(D)
-    L = np.array(L)   
-    
-    D = preprocessing.scale(D)
-    
-    return D, L
-
-def BuildData4(zigzag, returns, lag):
-    
-    D = []
-    L = []
-  
-    N = len(returns) + 1
-    Count = len(zigzag['time'])
-
-    #returns = preprocessing.scale(returns)
-
-    for i in range(len(zigzag["time"])-1):
-        index0 = zigzag["time"][i + 0]
-        index1 = zigzag["time"][i + 1]
-
-        l = zigzag["label"][i + 0]
-        
-        for j in range(index0, index1):
-            r = returns[j-lag+1:j]            
-            if len(r) is not 0:
-                D.append(r)
-                L.append(l)
-            
-
-    S = {'data' : [], 'label' : []}
-    
-    S['data']  = np.array(D)
-    S['label'] = np.array(L)    
-
-    return S
+#        for j in range(index0, index1):
+#            r = returns[j-lag+1:j]            
+#            if len(r) is not 0:
+#                D.append(r)
+#                L.append(l)
+#            
+#
+#    S = {'data' : [], 'label' : []}
+#    
+#    S['data']  = np.array(D)
+#    S['label'] = np.array(L)    
+#
+#    return S
--- util/ZigZag.py
+++ util/ZigZag.py
@@ -1,58 +1,120 @@
-def ZigZag(data, minsize):
-    
-    N = 0
-    T = 0
-    Z = {'zigzag': [], 'time': [], 'label' : []}
-    
-    Max  = data[0]
-    Min  = data[0]
-    
-    Flag = True
-    
-    PriceLow = 0
-    PriceHigh = 0    
-        
-    while N < len(data):
-        
-        PriceLow = data[N]
-        PriceHigh = data[N]       
-        
+import math
+import matplotlib.pyplot as plt
 
-        if Flag:
-            
-            if PriceHigh > Max:             
-             
-             	T = N
-                Max = PriceHigh                
-                
-            elif (Max - PriceLow >= minsize):
-                
-                Z['time'].append(T)
-                Z['label'].append(-1)                
-                Z['zigzag'].append(Max)
+def ZigZag(price, minsize):
+    
+    R = []
+    N = len(price)
 
-                
-                T = N
-                Flag = False
-                Min = PriceLow                
-                
+    for i in range(N-1):
+        R.append(price[i+1] - price[i])
+
+    N = len(R)
+
+    pref = []
+    pref.append({'v' : R[0], 'i' : 1})
+
+    for i in range(1, N):
+        pref.append({'v' : pref[i-1]['v'] + R[i], 'i' : i + 1})
+
+    X = []
+    N = len(price)
+    
+    ZigZagInternal(X, pref, minsize, 0, len(pref))
+
+    X.sort(key = lambda tup: tup['tmin'])
+
+    Y = []
+    for i in range(len(X)-1):
+
+        X0 = X[i]
+        X1 = X[i+1]
+
+        Y.append({'tmin' : X0['tmax'], 'tmax' : X1['tmin'], 'label' : 0, 'slice' : 0})
+
+    Z = sorted(X + Y, key = lambda tup : tup['tmin'])
+
+
+    #### plot ##################################################################################
+
+    fig, ax = plt.subplots()
+    for i in range(len(Z)):
+
+        i0    = Z[i]['tmin']
+        i1    = Z[i]['tmax']
+        label = Z[i]['label']
+        slice = Z[i]['slice']
+
+        color = None
+
+        if label < 0:
+            color = 'red'
+        elif label > 0:
+            color = 'blue'
         else:
-               
-            if PriceLow < Min:
-                
-                T = N
-                Min = PriceLow                
-                    
-            elif (PriceHigh - Min >= minsize):
-                    
-                Z['time'].append(T)
-                Z['label'].append(1)                
-                Z['zigzag'].append(Min)
-                
-                T = N
-                Flag = True
-                Max = PriceHigh    
+            color = 'green'
+
+        ax.plot(range(i0, i1+1), price[i0 : i1+1], color = color)
+
+        print i0, i1, i1 - i0, label, slice, price[i0], price[i1], price[i1] - price[i0]        
+
+    plt.show()
+
+    ############################################################################################
     
-        N = N + 1    
+    S = 0
+    for i in range(len(Z)):
+
+        i0 = Z[i]['tmin']
+        i1 = Z[i]['tmax']
+
+        S  = S + (i1 - i0)
+
+    print N, S
+
+    ############################################################################################
 
     return Z
+
+def ZigZagInternal(Z, pref, minsize, pmin, pmax):
+    
+    Z0    = []
+    pref0 = pref[pmin:pmax]
+
+    count = len(pref0)
+    
+
+    for i in range(count):
+        for p in range(i, count-1):
+
+            slice = pref0[p + 1]['v'] - pref0[i]['v']
+            delta = pref0[p + 1]['i'] - pref0[i]['i']
+
+            if math.fabs(slice) > minsize:
+                Z0.append({'tmin' : pref0[i]['i'], 'tmax' : pref0[p + 1]['i'], 'delta' : delta, 'slice' : slice, 'label' : 1 if slice > 0 else -1})
+
+                break
+
+    count = len(Z0)
+
+    if count > 0:
+        minZ0 = Z0[0]
+
+        for i in range(count):
+            delta = Z0[i]['delta']
+
+            if delta < minZ0['delta']:                
+                minZ0 = Z0[i]
+
+        Z.append(minZ0)
+
+        zmin = minZ0['tmin'] - 1
+        zmax = minZ0['tmax']
+
+        if zmin - pmin > 1:
+            ZigZagInternal(Z, pref, minsize, pmin, zmin)
+
+        if pmax - zmax > 1:
+            ZigZagInternal(Z, pref, minsize, zmax, pmax)
+
+    pass
